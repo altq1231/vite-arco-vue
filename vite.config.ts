e9112199635d8d85c7f06eb9668f137e42ad9267
altq <zhong.li@united-imaging.com>
2024-02-02 10:04:29 +0800

    add

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  plugins: [vue(), vueJsx()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+  server: {
+    port: 3300,
+  },
+});
